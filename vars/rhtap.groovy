cb5e7c23dbe860d024327ea9f0fb41ca9462fb50
gitops-update <rhtap@noreplay.com>
2024-07-15 10:36:26 -0400

    dbg

--- vars/rhtap.groovy
+++ vars/rhtap.groovy
@@ -1,4 +1,19 @@
 
+// vars/get_resource_dir.groovy
+import groovy.transform.SourceURI
+import java.nio.file.Path
+import java.nio.file.Paths
+
+class ScriptSourceUri {
+    @SourceURI
+    static URI uri
+}
+
+def call() {
+    Path scriptLocation = Paths.get(ScriptSourceUri.uri)
+    return scriptLocation.getParent().getParent().resolve('resources').toString()
+}
+
 def info(message) {
     echo "INFO: ${message}" 
 }
