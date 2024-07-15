4361e87393258b4ec6a37df40cf20fd6345cf904
gitops-update <rhtap@noreplay.com>
2024-07-15 10:54:09 -0400

    echo "WOO HOO WOO HOO "

--- resources/init.sh
+++ resources/init.sh
@@ -1,3 +1,20 @@
 
+echo "WOO HOO WOO HOO " 
+echo "WOO HOO WOO HOO " 
+echo "WOO HOO WOO HOO " 
+echo "WOO HOO WOO HOO " 
+echo "WOO HOO WOO HOO " 
+echo "WOO HOO WOO HOO " 
+echo "WOO HOO WOO HOO " 
+echo "WOO HOO WOO HOO " 
 
 echo "/home/john/dev/rhtap-jenkins/resources/init.sh Init from Shared Library " 
+
+
+echo "WOO HOO WOO HOO " 
+echo "WOO HOO WOO HOO " 
+echo "WOO HOO WOO HOO " 
+echo "WOO HOO WOO HOO " 
+echo "WOO HOO WOO HOO " 
+echo "WOO HOO WOO HOO " 
+echo "WOO HOO WOO HOO " 
