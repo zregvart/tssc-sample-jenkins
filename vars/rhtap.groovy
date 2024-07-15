d19f7e7925f77c6e3a18bbc9c23cafe36abb57e7
gitops-update <rhtap@noreplay.com>
2024-07-15 10:58:44 -0400

    run init

--- vars/rhtap.groovy
+++ vars/rhtap.groovy
@@ -3,14 +3,15 @@ def info(message) {
     echo "XX INFO: ${message}" 
 }
 
-def init( ) {  
-    echo "XX2" 
-    sh "pwd"
-    echo " "  
-    sh "ls -al " 
-    echo " "  
-    sh "ls -al .." 
-    echo " " 
-    sh "ls -al ../.." 
-}  
+def init( ) {
+    rhtap.info ("Loading libraryResource('init.sh')")
+    contents = libraryResource('init.sh')
+    rhtap.info ("------")  
+    rhtap.info ("Contents of init.sh")
+    printf("<%s>\n", contents); 
+    rhtap.info ("------")  
+    rhtap.info ("Running init.sh")
+    sh contents  
+    rhtap.info ("-----------------------")
+}   
 
