b60af9ab92628b859100427945e5dc5c6f3454a8
Zoran Regvart <zoran@regvart.com>
2024-11-13 11:40:28 +0100

    try

--- vars/rhtap.groovy
+++ vars/rhtap.groovy
@@ -97,10 +97,8 @@ def env() {
     echo("---------------------------")
     echo(currentBuild.rawBuild.parent.definition.dump())
     echo("---------------------------")
-    echo(currentBuild.getAction(jenkins.scm.api.SCMRevisionAction.class))
-    echo("---------------------------")
     [
         pipeline_path: currentBuild.rawBuild.parent.definition.scriptPath,
-        pipeline_rev: currentBuild.getAction(jenkins.scm.api.SCMRevisionAction.class).revision.head.name
+        pipeline_rev: currentBuild.rawBuild.getAction(jenkins.scm.api.SCMRevisionAction.class).revision.head.name
     ]
 }
